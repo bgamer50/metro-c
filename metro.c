adec47de1dea131e654eded9c29622238b881ecb
Alex Barghi <abarghi@abarghi.com>
2015-05-29 12:02:49 -0400

    fully-working metro map

--- metro.c
+++ metro.c
@@ -7,11 +7,14 @@
 Station stations[91];
 int NUMBER_OF_STATIONS;
 int main(char* argv[]) {
-	int k;
+	int k, i;
 	parseMap();
 	parseDistances();
-	for(k = 0; k < 91; k++) {
-		printf("%s %d %d\n", stations[k].name, stations[k].neighbors[0], 0);
+	for(k = 1; k < 92; k++) {
+		printf("\n%s | ", stations[k].name);
+		for(i = 0; i < stations[k].neighborCount; i++) {
+			printf("[%s, %d]; ", stations[stations[k].neighbors[i]].name, stations[k].neighborWeights[i]);
+		}
 	}
 	printf("\nhello\n");
 }
@@ -24,7 +27,7 @@ void parseMap() {
 	int* g = malloc(sizeof(int));
 	int* h = malloc(sizeof(int));
 	fscanf(map, "%d", g);
-	int k = 0;
+	int k = 1;
 	while(fscanf(map, "%s %s %d %d", e, f, g, h) != EOF) {
 		strcpy(stations[k].name, e);
 		stations[k].line = f;
@@ -38,10 +41,6 @@ void parseMap() {
 	NUMBER_OF_STATIONS = *g;
 }
 
-Station* int2Station(int k) {
-	return &stations[k - 1];
-}
-
 void parseDistances() {
 	FILE* dist = fopen("metrodist.data", "r");
 	int* e = malloc(sizeof(int));
@@ -49,8 +48,10 @@ void parseDistances() {
 	int* g = malloc(sizeof(int));
 	Station E; Station F;
 	while(fscanf(dist, "%d %d %d", e, f, g) != EOF) {
-		stations[*e - 1].neighbors[stations[*e - 1].neighborCount++] = *f;
-		stations[*f - 1].neighbors[stations[*f - 1].neighborCount++] = *e;
+		stations[*e].neighbors[stations[*e].neighborCount++] = *f;
+		stations[*f].neighbors[stations[*f].neighborCount++] = *e;
 		//printf("%d %d\n", *f, *e);
+		stations[*e].neighborWeights[stations[*e].neighborCount - 1] = *g;
+		stations[*f].neighborWeights[stations[*f].neighborCount - 1] = *g;
 	}
 }
--- station.h
+++ station.h
@@ -2,6 +2,7 @@ typedef struct stn {
 	int ID;
 	char name[50];
 	int neighbors[4];
+	int neighborWeights[4];
 	int neighborCount;
 	char* line; //yellow rush is Z.
 	bool transfer;
